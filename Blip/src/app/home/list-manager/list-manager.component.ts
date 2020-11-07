0cce5d2cca9e90465f2bc488c83799fc0abdad2f
joao <jpls2@cesar.org.br>
2020-11-06 22:23:18 -0300

    start project

---
+++ Blip/src/app/home/list-manager/list-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-manager',
+  templateUrl: './list-manager.component.html',
+  styleUrls: ['./list-manager.component.scss']
+})
+export class ListManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
